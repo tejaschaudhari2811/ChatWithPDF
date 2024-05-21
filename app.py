f860deb20d8b084777d47046644498b4d1217c10
Tejas Chaudhari <tmcv0.01@gmail.com>
2024-05-21 11:04:01 +0200

    Update breaking changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from langchain_openai import AzureChatOpenAI
 from dotenv import load_dotenv
 from langchain_openai.embeddings import AzureOpenAIEmbeddings
-from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
